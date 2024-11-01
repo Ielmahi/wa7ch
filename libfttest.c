11d5e689f01554480b9c64c2cb7f10a45eb84b61
ielmahi <vodkailias@gmail.com>
2024-11-01 21:01:16 +0100

    update

--- libfttest.c
+++ libfttest.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main()
 {
 	printf("%s\n",ft_strnstr(NULL,"ilias",0));
 	printf("%zu\n",ft_strlcat(NULL,"ilias",0));
 	printf("%d\n",ft_strncmp(NULL,"ilias",0));
+	printf("%zu\n",ft_strlcpy(NULL,"ilias",0));
 	ft_calloc(10000000000000000,1000000000000000);
 }
